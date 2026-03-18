9dc1778cf50f4a666d9e22c0922540b5d304e07c
Jorge Oropeza <yorch4922@gmail.com>
2026-03-17 21:21:19 -0700

    fix: add use client directive to case study page

--- src/app/(case-studies)/projects/streamlining-urban-development/page.tsx
+++ src/app/(case-studies)/projects/streamlining-urban-development/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { Users, Zap, Award, PencilRuler, ArrowRightCircle, Database, Layers, Search, CheckCircle } from "lucide-react";
