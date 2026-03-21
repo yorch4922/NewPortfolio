f5ac4e726c69fcca13488b8b515f82b95bc68363
Jorge Oropeza <yorch4922@gmail.com>
2026-03-21 09:08:54 -0700

    feat:Clarity Instalation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Nunito, Merriweather } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const nunito = Nunito({
@@ -26,6 +27,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "vysxj4i9qm");
+        `}
+      </Script>
       <body
         className={`${nunito.variable} ${merriweather.variable} font-sans antialiased`}
       >
